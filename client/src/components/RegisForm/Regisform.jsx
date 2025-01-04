cfff47a82af7a0034364cc29379e65fbbac73e4d
Sandhya <stefanus.sandhya@gmail.com>
2025-01-04 12:01:02 +0700

    fix regisform

--- client/src/components/RegisForm/Regisform.jsx
+++ client/src/components/RegisForm/Regisform.jsx
@@ -13,34 +13,36 @@ const Regisform = () => {
     navigate("/login"); // Navigasi ke halaman login
   };
   return (
-    <div className="registerform">
-      <form onSubmit={handleRegister}>
-        <h1>Register</h1>
-        <div className="inputbox">
-          <input type="text" placeholder="Username" required />
-          <FaUserAlt className="icon" />
-        </div>
-        <div className="inputbox">
-          <input type="email" placeholder="Email" required />
-          <FaKey className="icon" />
-        </div>
-        <div className="inputbox">
-          <input type="password" placeholder="Password" required />
-          <FaKey className="icon" />
-        </div>
-        <div className="remember-forgot">
-          <label>
-            <input type="checkbox" />
-            Remember me
-          </label>
-        </div>
-        <button type="submit">Register</button>
-        <div className="register-link">
-          <p>
-            Have an account? <a href="login">Login</a>
-          </p>
-        </div>
-      </form>
+    <div className="registerbody">
+      <div className="registerform">
+        <form onSubmit={handleRegister}>
+          <h1>Register</h1>
+          <div className="inputbox">
+            <input type="text" placeholder="Username" required />
+            <FaUserAlt className="icon" />
+          </div>
+          <div className="inputbox">
+            <input type="email" placeholder="Email" required />
+            <FaKey className="icon" />
+          </div>
+          <div className="inputbox">
+            <input type="password" placeholder="Password" required />
+            <FaKey className="icon" />
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+          </div>
+          <button type="submit">Register</button>
+          <div className="register-link">
+            <p>
+              Have an account? <a href="login">Login</a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
